c6fb382287130180f8e3545b1c9f2c6cb5de3dd4
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-08-02 17:22:08 +0900

    Add example transaction requests for unit tests and apply it

---
+++ tests/example_tx_requests.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2018 theloop Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TEST_REQUEST_TRANSFER_ICX = {
+    "jsonrpc": "2.0",
+    "method": "icx_sendTransaction",
+    "id": 1234,
+    "params": {
+        "version": "0x3",
+        "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
+        "to": "hx5bfdb090f43a808005ffc27c25b213145e80b7cd",
+        "value": "0xde0b6b3a7640000",
+        "stepLimit": "0x12345",
+        "timestamp": "0x563a6cf330136",
+        "nid": "0x3f",
+        "nonce": "0x1",
+        "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA="
+    }
+}
+
+TEST_REQUEST_SCORE_FUNCTION_CALL = {
+    "jsonrpc": "2.0",
+    "method": "icx_sendTransaction",
+    "id": 1234,
+    "params": {
+        "version": "0x3",
+        "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
+        "to": "cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32",
+        "stepLimit": "0x12345",
+        "timestamp": "0x563a6cf330136",
+        "nid": "0x3f",
+        "nonce": "0x1",
+        "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA=",
+        "dataType": "call",
+        "data": {
+            "method": "transfer",
+            "params": {
+                "to": "hxab2d8215eab14bc6bdd8bfb2c8151257032ecd8b",
+                "value": "0x1"
+            }
+        }
+    }
+}
+
+TEST_REQUEST_SCORE_ISNTALL = {
+    "jsonrpc": "2.0",
+    "method": "icx_sendTransaction",
+    "id": 1234,
+    "params": {
+        "version": "0x3",
+        "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
+        "to": "cx0000000000000000000000000000000000000000",
+        "stepLimit": "0x12345",
+        "timestamp": "0x563a6cf330136",
+        "nid": "0x3f",
+        "nonce": "0x1",
+        "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA=",
+        "dataType": "deploy",
+        "data": {
+            "contentType": "application/zip",
+            "content": "0x1867291283973610982301923812873419826abcdef91827319263187263a7326e...",
+            "params": {
+                "name": "ABCToken",
+                "symbol": "abc",
+                "decimals": "0x12"
+            }
+        }
+    }
+}
+
+TEST_REQUEST_SCORE_UPDATE = {
+    "jsonrpc": "2.0",
+    "method": "icx_sendTransaction",
+    "id": 1234,
+    "params": {
+        "version": "0x3",
+        "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
+        "to": "cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32",
+        "stepLimit": "0x12345",
+        "timestamp": "0x563a6cf330136",
+        "nid": "0x3f",
+        "nonce": "0x1",
+        "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA=",
+        "dataType": "deploy",
+        "data": {
+            "contentType": "application/zip",
+            "content": "0x1867291283973610982301923812873419826abcdef91827319263187263a7326e...",
+            "params": {
+                "amount": "0x1234"
+            }
+        }
+    }
+}
+
+TEST_REQUEST_SEND_MESSAGE = {
+    "jsonrpc": "2.0",
+    "method": "icx_sendTransaction",
+    "id": 1234,
+    "params": {
+        "version": "0x3",
+        "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
+        "to": "hxbe258ceb872e08851f1f59694dac2558708ece11",
+        "stepLimit": "0x12345",
+        "timestamp": "0x563a6cf330136",
+        "nid": "0x3f",
+        "nonce": "0x1",
+        "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA=",
+        "dataType": "message",
+        "data": "0x4c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e7365637465747572206164697069736963696e6720656c69742c2073656420646f20656975736d6f642074656d706f7220696e6369646964756e74207574206c61626f726520657420646f6c6f7265206d61676e6120616c697175612e20557420656e696d206164206d696e696d2076656e69616d2c2071756973206e6f737472756420657865726369746174696f6e20756c6c616d636f206c61626f726973206e69736920757420616c697175697020657820656120636f6d6d6f646f20636f6e7365717561742e2044756973206175746520697275726520646f6c6f7220696e20726570726568656e646572697420696e20766f6c7570746174652076656c697420657373652063696c6c756d20646f6c6f726520657520667567696174206e756c6c612070617269617475722e204578636570746575722073696e74206f6363616563617420637570696461746174206e6f6e2070726f6964656e742c2073756e7420696e2063756c706120717569206f666669636961206465736572756e74206d6f6c6c697420616e696d20696420657374206c61626f72756d2e"
+    }
+}
--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -16,121 +16,30 @@
 import unittest
 import hashlib
 
-from IconService.libs.serializer import generate_tx_hash, serialize_params_to_message_hash
+from IconService.libs.serializer import serialize_tx_request_to_message_hash
+from .example_tx_requests import TEST_REQUEST_TRANSFER_ICX, TEST_REQUEST_SCORE_FUNCTION_CALL
 
 
 class TestSerializer(unittest.TestCase):
 
     def test_for_serialize_case_for_sending_normal_tx(self):
         """Test when serializer serializes perfectly in this case when is normal send transaction."""
-        tx_request = {
-                        "jsonrpc": "2.0",
-                        "method": "icx_sendTransaction",
-                        "id": 1234,
-                        "params": {
-                            "version": "0x3",
-                            "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
-                            "to": "hx5bfdb090f43a808005ffc27c25b213145e80b7cd",
-                            "value": "0xde0b6b3a7640000",
-                            "stepLimit": "0x12345",
-                            "timestamp": "0x563a6cf330136",
-                            "nonce": "0x1",
-                            "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA="
-                            }
-                     }
-
-        params_of_tx_request = tx_request["params"]
-        correct_serialized_params = "icx_sendTransaction.from.hxbe258ceb872e08851f1f59694dac2558708ece11." \
-                                    "nonce.0x1.stepLimit.0x12345.timestamp.0x563a6cf330136.to.hx5bfdb090f" \
-                                    "43a808005ffc27c25b213145e80b7cd.value.0xde0b6b3a7640000.version.0x3"
-
-        self.assertEqual(correct_serialized_params.encode(), serialize_params_to_message_hash(params_of_tx_request))
+        tx_request = TEST_REQUEST_TRANSFER_ICX
+        correct_serialized_params = "icx_sendTransaction.from.hxbe258ceb872e08851f1f59694dac2558708ece11.nid.0x3f." \
+                                    "nonce.0x1.stepLimit.0x12345.timestamp.0x563a6cf330136.to.hx5bfdb090f43a808005" \
+                                    "ffc27c25b213145e80b7cd.value.0xde0b6b3a7640000.version.0x3"
+        self.assertEqual(hashlib.sha3_256(correct_serialized_params.encode()).digest(),
+                         serialize_tx_request_to_message_hash(tx_request))
 
     def test_for_serialize_case_for_calling(self):
         """Test when serializer serializes perfectly in this case when dataType is call."""
-        tx_request = {
-                        "jsonrpc": "2.0",
-                        "method": "icx_sendTransaction",
-                        "id": 1234,
-                        "params": {
-                            "version": "0x3",
-                            "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
-                            "to": "cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32",
-                            "stepLimit": "0x12345",
-                            "timestamp": "0x563a6cf330136",
-                            "nonce": "0x1",
-                            "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA=",
-                            "dataType": "call",
-                            "data": {
-                                "method": "transfer",
-                                "params": {
-                                    "to": "hxab2d8215eab14bc6bdd8bfb2c8151257032ecd8b",
-                                    "value": "0x1"
-                                }
-                            }
-                        }
-                    }
-
-        params_of_tx_request = tx_request["params"]
+        tx_request = TEST_REQUEST_SCORE_FUNCTION_CALL
         correct_serialized_params = "icx_sendTransaction.data.{method.transfer.params.{to.hxab2d8215eab14bc6bdd8b" \
                                     "fb2c8151257032ecd8b.value.0x1}}.dataType.call.from.hxbe258ceb872e08851f1f596" \
-                                    "94dac2558708ece11.nonce.0x1.stepLimit.0x12345.timestamp.0x563a6cf330136.to.c" \
-                                    "xb0776ee37f5b45bfaea8cff1d8232fbb6122ec32.version.0x3"
-        self.assertEqual(correct_serialized_params.encode(), serialize_params_to_message_hash(params_of_tx_request))
-
-    def test_for_checking_hash_is_correct_case_for_sending_normal_tx(self):
-        """
-        Test for checking the transaction hash is correct successfully
-        in this case when is normal send transaction.
-        """
-        tx_request = {
-                        "jsonrpc": "2.0",
-                        "method": "icx_sendTransaction",
-                        "id": 1234,
-                        "params": {
-                            "version": "0x3",
-                            "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
-                            "to": "hx5bfdb090f43a808005ffc27c25b213145e80b7cd",
-                            "value": "0xde0b6b3a7640000",
-                            "stepLimit": "0x12345",
-                            "timestamp": "0x563a6cf330136",
-                            "nonce": "0x1",
-                            "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA="
-                            }
-                     }
-        params_of_tx_request = tx_request["params"]
-        tx_hash = generate_tx_hash(params_of_tx_request)
-        correct_tx_hash = hashlib.sha3_256(serialize_params_to_message_hash(params_of_tx_request)).hexdigest()
-        self.assertEqual(tx_hash, correct_tx_hash)
-
-    def test_for_checking_hash_is_correct_case_for_calling(self):
-        """Test for checking the transaction hash is correct successfully in this case when dataType is call."""
-        tx_request = {
-                        "jsonrpc": "2.0",
-                        "method": "icx_sendTransaction",
-                        "id": 1234,
-                        "params": {
-                            "version": "0x3",
-                            "from": "hxbe258ceb872e08851f1f59694dac2558708ece11",
-                            "to": "cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32",
-                            "stepLimit": "0x12345",
-                            "timestamp": "0x563a6cf330136",
-                            "nonce": "0x1",
-                            "signature": "VAia7YZ2Ji6igKWzjR2YsGa2m53nKPrfK7uXYW78QLE+ATehAVZPC40szvAiA6NEU5gCYB4c4qaQzqDh2ugcHgA=",
-                            "dataType": "call",
-                            "data": {
-                                "method": "transfer",
-                                "params": {
-                                    "to": "hxab2d8215eab14bc6bdd8bfb2c8151257032ecd8b",
-                                    "value": "0x1"
-                                }
-                            }
-                        }
-                    }
-        params_of_tx_request = tx_request["params"]
-        tx_hash = generate_tx_hash(params_of_tx_request)
-        correct_tx_hash = hashlib.sha3_256(serialize_params_to_message_hash(params_of_tx_request)).hexdigest()
-        self.assertEqual(tx_hash, correct_tx_hash)
+                                    "94dac2558708ece11.nid.0x3f.nonce.0x1.stepLimit.0x12345.timestamp.0x563a6cf33" \
+                                    "0136.to.cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32.version.0x3"
+        self.assertEqual(hashlib.sha3_256(correct_serialized_params.encode()).digest(),
+                         serialize_tx_request_to_message_hash(tx_request))
 
 
 if __name__ == "__main__":
