3c7e7bd9a3aabc2cd37af2c2bb46a2802dea1cc4
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-08-02 17:19:23 +0900

    Fix unit tests for verifying a method to make and recover a signature

--- tests/test_signer.py
+++ tests/test_signer.py
@@ -14,59 +14,43 @@
 # limitations under the License.
 
 import unittest
-import base64
 import hashlib
 
-from IconService.libs.signer import IcxSigner
+from IconService.libs.serializer import serialize_tx_request_to_message_hash
+from IconService.libs.signer import sign
 from secp256k1 import PrivateKey
+from .example_tx_requests import TEST_REQUEST_SCORE_FUNCTION_CALL, \
+    TEST_REQUEST_SCORE_ISNTALL, TEST_REQUEST_SCORE_UPDATE, TEST_REQUEST_SEND_MESSAGE, \
+    TEST_REQUEST_TRANSFER_ICX
 
 
 class TestIcxSigner(unittest.TestCase):
-    def setUp(self):
-        self.test_private_key = PrivateKey()
-        self.signer = IcxSigner(self.test_private_key.private_key)
 
-        m = hashlib.sha256()
-        m.update(b'message_for_test')
-        # prepare massage msg_hash
-        self.hashed_message = m.digest()
+    def test_verify_recoverable_sign(self):
+        """Verifies recovering a signature."""
 
-        # check if signature which sign_recoverable method made is valid
-        # use ecdsa_verify. before verify signature, convert sign (recoverable_sig -> normal_sig)
-        # check secp256k1 doc: https://github.com/ludbb/secp256k1-py
+        test_requests = [TEST_REQUEST_TRANSFER_ICX, TEST_REQUEST_SCORE_FUNCTION_CALL,
+                         TEST_REQUEST_SEND_MESSAGE, TEST_REQUEST_SCORE_UPDATE, TEST_REQUEST_SCORE_ISNTALL]
 
-    def test_sign_recoverable_verify_sig(self):
-        # get sign, recovery
-        sign, recovery_id = self.signer.sign_recoverable(self.hashed_message)
+        for request in test_requests:
+            # serialize a signature
+            private_key_object = PrivateKey()
+            msg_hash_bytes = serialize_tx_request_to_message_hash(request)
+            sign_bytes = sign(msg_hash_bytes, private_key_object)
 
-        # Convert recoverable sig to normal sig
-        deserialized_recoverable_sig = self.test_private_key.ecdsa_recoverable_deserialize(sign, recovery_id)
-        normal_sig = self.test_private_key.ecdsa_recoverable_convert(deserialized_recoverable_sig)
+            # deserialize a signature
+            recoverable_sign = private_key_object.ecdsa_recoverable_deserialize(sign_bytes[0:64], sign_bytes[64])
+            sign_ = private_key_object.ecdsa_recoverable_convert(recoverable_sign)
+            # Verify a signature with a public key
+            self.assertTrue(private_key_object.pubkey.ecdsa_verify(msg_hash_bytes, sign_, raw=True))
 
-        # Check sig
-        self.assertTrue(self.test_private_key.pubkey.ecdsa_verify(self.hashed_message, normal_sig, raw=True))
+            # Verify a signature when an message is invalid
+            invalid_msg_hash = hashlib.sha3_256(f'invalid message'.encode()).digest()
+            self.assertFalse(private_key_object.pubkey.ecdsa_verify(invalid_msg_hash, sign_, raw=True))
 
-        # Verify using invalid message
-        m = hashlib.sha256()
-        m.update(b'invalid message')
-        invalid_message = m.digest()
-        self.assertFalse(self.test_private_key.pubkey.ecdsa_verify(invalid_message, normal_sig, raw=True))
-
-        # Verify using invalid private key
-        invalid_privateKey = PrivateKey()
-        self.assertFalse(invalid_privateKey.pubkey.ecdsa_verify(self.hashed_message, normal_sig, raw=True))
-
-    def test_sign_base64_encode(self):
-        # make signature
-        encoded_sign = self.signer.sign(self.hashed_message)
-        decoded_sign = base64.b64decode(encoded_sign)
-
-        actual_id = int.from_bytes(decoded_sign[-1:], byteorder='big')
-        actual_sig = decoded_sign[:len(decoded_sign) - 1]
-
-        expected_signature, expected_recovery_id = self.signer.sign_recoverable(self.hashed_message)
-        self.assertEqual(actual_id, expected_recovery_id)
-        self.assertEqual(actual_sig, expected_signature)
+            # Verify a signature when a private key is invalid
+            invalid_private_key = PrivateKey()
+            self.assertFalse(invalid_private_key.pubkey.ecdsa_verify(msg_hash_bytes, sign_, raw=True))
 
 
 if __name__ == "__main__":
