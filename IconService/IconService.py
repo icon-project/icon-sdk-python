9983328744f1d262f63ccc92e71823b9a2b4f653
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-08-08 18:23:48 +0900

    Implement a `get_block` method
    Checks a parameter-height or block hash or latest and calls each api method

--- IconService/IconService.py
+++ IconService/IconService.py
@@ -13,17 +13,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from IconService.providers.http_provider import HTTPProvider, Provider
+from IconService.utils.validation import is_hex_block_height, \
+    is_hex_block_hash, is_predefined_block_value
+
 
 class IconService:
 
-    def get_block(self, height_or_block_hash_or_latest: int):
+    def __init__(self, provider: Provider):
+        self.__provider = provider
+
+    def get_block(self, value: str):
         """
         If param is height, it is equivalent to icx_getBlockByHeight.
         Or block hash, it is equivalent to icx_getBlockByHash.
         Or string value same as `latest`, it is equivalent to icx_getLastBlock.
 
-        :return: block
+        :param value: height or hash or `latest`. type(str)
+        :return response: block information.
         """
+        # by height
+        if is_hex_block_height(value):
+            params = {'height': value}
+            response = self.__provider.make_request('icx_getBlockByHeight', params)
+        # by hash
+        elif is_hex_block_hash(value):
+            params = {'hash': value}
+            response = self.__provider.make_request('icx_getBlockByHash', params)
+        # by value
+        elif is_predefined_block_value(value):
+            response = self.__provider.make_request('icx_getLastBlock')
+        else:
+            raise TypeError("ERROR")
+
+        return response
 
     def get_total_supply(self):
         """It is equivalent to icx_getTotalSupply.
@@ -37,8 +60,6 @@ class IconService:
         :param address: str
         :return:d
         """
-
-
 
     def get_score_api(self, address: str):
         """It is equivalent to icx_getScoreApi.
