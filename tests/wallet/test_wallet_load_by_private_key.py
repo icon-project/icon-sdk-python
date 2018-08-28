72f992909a0634a378db4989b14038f015a970cc
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-08-28 16:48:46 +0900

    Revise unit tests related that private key data type should be bytes

--- tests/wallet/test_wallet_load_by_private_key.py
+++ tests/wallet/test_wallet_load_by_private_key.py
@@ -17,6 +17,7 @@ from unittest import TestCase, main
 from IconService.wallet.wallet import KeyWallet
 from secp256k1 import PrivateKey
 from IconService.utils.validation import is_wallet_address
+from IconService.exception import DataTypeException
 
 
 class TestWalletLoadByPrivateKey(TestCase):
@@ -26,38 +27,32 @@ class TestWalletLoadByPrivateKey(TestCase):
 
         # Creates a wallet.
         private_key_object = PrivateKey()
-        hex_private_key = private_key_object.private_key.hex()
-        wallet1 = KeyWallet.load(hex_private_key)
+        private_key = private_key_object.private_key
+        wallet1 = KeyWallet.load(private_key)
 
         # Checks a private key as same.
-        self.assertEqual(hex_private_key, wallet1.get_private_key())
+        self.assertEqual(private_key.hex(), wallet1.get_private_key())
 
         # Checks a wallet's address is correct.
         self.assertTrue(is_wallet_address(wallet1.get_address()))
 
         # Creates the other wallet.
         private_key_object2 = PrivateKey()
-        hex_private_key2 = private_key_object2.private_key.hex()
-        wallet2 = KeyWallet.load(hex_private_key2)
+        private_key2 = private_key_object2.private_key
+        wallet2 = KeyWallet.load(private_key2)
 
         # Checks a private key as same.
-        self.assertEqual(hex_private_key2, wallet2.get_private_key())
+        self.assertEqual(private_key2.hex(), wallet2.get_private_key())
 
         # Checks a wallet's address is correct.
         self.assertTrue(is_wallet_address(wallet2.get_address()))
 
-        self.assertNotEqual(hex_private_key2, hex_private_key)
+        self.assertNotEqual(private_key2, private_key)
 
     def test_wallet_load_by_invalid_private_key(self):
         """A wallet loads by a wrong private key. It will fail."""
-
-        try:
-            # Given, When
-            wallet = KeyWallet.load("71fc378d3a3fb92b57474af156f9d277c9b60a923a1db75575b1cc")
-
-        # Then
-        except TypeError:
-            self.assertTrue(True)
+        self.assertRaises(DataTypeException, KeyWallet.load,
+                          bytes.fromhex("71fc378d3a3fb92b57474af156f9d277c9b60a923a1db75575b1cc"))
 
 
 if __name__ == "__main__":
