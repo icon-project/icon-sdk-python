ff5861f048c9e2cf8cf0db9652f4ac87e5d770a3
byungkwon bae <endlessgate1@gmail.com>
2021-02-01 15:30:35 +0900

    Merge branch 'master' into develop

--- tests/api_send/test_send_message.py
+++ tests/api_send/test_send_message.py
@@ -12,35 +12,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json
 
-import requests_mock
+from time import sleep
 
 from iconsdk.builder.transaction_builder import MessageTransactionBuilder
 from iconsdk.exception import JSONRPCException, DataTypeException
 from iconsdk.signed_transaction import SignedTransaction
 from iconsdk.utils.validation import is_message_transaction, is_T_HASH
 from tests.api_send.test_send_super import TestSendSuper
-from tests.example_config import BASE_DOMAIN_URL_V3_FOR_TEST
 
 
 class TestSendMessage(TestSendSuper):
+
     def test_send_message(self):
+        sleep_time = 2
+
+        # Checks if making an instance of message transaction correctly
         message_transaction = MessageTransactionBuilder() \
             .from_(self.setting["from"]) \
             .to(self.setting["to"]) \
             .step_limit(self.setting["step_limit"]) \
             .nid(self.setting["nid"]) \
             .nonce(self.setting["nonce"]) \
-            .data(self.setting["data"]) \
-            .timestamp(self.setting["timestamp"]) \
-            .build()
+            .data(self.setting["data"]).build()
         tx_dict = SignedTransaction.convert_tx_to_jsonrpc_request(message_transaction)
         self.assertTrue(is_message_transaction(tx_dict))
+
         # Checks if sending transaction correctly
         signed_transaction = SignedTransaction(message_transaction, self.wallet)
-<<<<<<< HEAD
-=======
         result = self.icon_service.send_transaction(signed_transaction)
         self.assertTrue(is_T_HASH(result))
 
@@ -63,39 +62,7 @@ class TestSendMessage(TestSendSuper):
 
         # Checks the block's transaction
         self.assertEqual(block["confirmed_transaction_list"][1]["data"], self.setting["data"])
->>>>>>> master
 
-        with requests_mock.Mocker() as m:
-            tx_hash: str = "0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238"
-            expected_result: dict = {
-                "jsonrpc": "2.0",
-                "result": tx_hash,
-                "id": 1234
-            }
-            expected_request_body: dict = {
-                'id': 1234,
-                'jsonrpc': '2.0',
-                'method': 'icx_sendTransaction',
-                'params': {
-                    'data': self.setting["data"],
-                    'dataType': 'message',
-                    'from': self.setting["from"],
-                    'nid': hex(self.setting["nid"]),
-                    'nonce': hex(self.setting["nonce"]),
-                    'signature': signed_transaction.signed_transaction_dict["signature"],
-                    'stepLimit': hex(self.setting["step_limit"]),
-                    'timestamp': hex(self.setting["timestamp"]),
-                    'to': self.setting["to"],
-                    'version': '0x3'
-                }
-            }
-            m.post(f"{BASE_DOMAIN_URL_V3_FOR_TEST}/api/v3", json=expected_result)
-            result = self.icon_service.send_transaction(signed_transaction)
-            self.assertTrue(is_T_HASH(result))
-            actual_request_body = json.loads(m._adapter.last_request.text)
-            self.assertEqual(expected_request_body, actual_request_body)
-
-    def test_send_message_invalid_data1(self):
         # When data is not hex string
         message_transaction = MessageTransactionBuilder() \
             .from_(self.setting["from"]) \
@@ -104,86 +71,26 @@ class TestSendMessage(TestSendSuper):
             .nid(self.setting["nid"]) \
             .nonce(self.setting["nonce"]) \
             .data("test")
+
         # Raise DataTypeException
         self.assertRaises(DataTypeException, message_transaction.build)
 
-    def test_send_message_invalid_data2(self):
-        message_transaction = MessageTransactionBuilder()\
-            .from_(self.setting["from"]) \
-            .to(self.setting["to"][2:]) \
-            .step_limit(self.setting["step_limit"]) \
-            .nid(self.setting["nid"]) \
-            .data(self.setting["data"]) \
-            .timestamp(self.setting["timestamp"]) \
-            .build()
-
+        # When address is wrong
+        message_transaction = MessageTransactionBuilder().from_(self.setting["from"]).to(self.setting["to"][2:]) \
+            .step_limit(self.setting["step_limit"]).nid(self.setting["nid"]).data(self.setting["data"]).build()
         signed_transaction = SignedTransaction(message_transaction, self.wallet)
-        with requests_mock.Mocker() as m:
-            expected_result: dict = {
-                "jsonrpc": "2.0",
-                "error": {
-                    "code": -32000,
-                    "message": "Server error"
-                },
-                "id": 1234
-            }
-            expected_request_body: dict = {
-                'id': 1234,
-                'jsonrpc': '2.0',
-                'method': 'icx_sendTransaction',
-                'params': {
-                    'data': self.setting["data"],
-                    'dataType': 'message',
-                    'from': self.setting["from"],
-                    'nid': hex(self.setting["nid"]),
-                    'signature': signed_transaction.signed_transaction_dict["signature"],
-                    'stepLimit': hex(self.setting["step_limit"]),
-                    'timestamp': hex(self.setting["timestamp"]),
-                    'to': self.setting["to"][2:],
-                    'version': '0x3'
-                }
-            }
-            m.post(f"{BASE_DOMAIN_URL_V3_FOR_TEST}/api/v3", status_code=500, json=expected_result)
-            self.assertRaises(JSONRPCException, self.icon_service.send_transaction, signed_transaction)
-            actual_request_body = json.loads(m._adapter.last_request.text)
-            self.assertEqual(expected_request_body, actual_request_body)
+        self.assertRaises(JSONRPCException, self.icon_service.send_transaction, signed_transaction)
 
-    def test_send_message_invalid_data3(self):
-        message_transaction = MessageTransactionBuilder() \
-            .from_(self.setting["from"]) \
-            .to(self.setting["to"]) \
-            .step_limit(self.setting["step_limit"]) \
-            .data(self.setting["data"]) \
-            .timestamp(self.setting["timestamp"]) \
-            .build()
-
+        # When not having a required property, nid
+        message_transaction = MessageTransactionBuilder().from_(self.setting["from"]).to(self.setting["to"][2:]) \
+            .step_limit(self.setting["step_limit"]).data(self.setting["data"]).build()
         signed_transaction = SignedTransaction(message_transaction, self.wallet)
-        with requests_mock.Mocker() as m:
-            expected_result: dict = {
-                "jsonrpc": "2.0",
-                "error": {
-                    "code": -32000,
-                    "message": "Server error"
-                },
-                "id": 1234
-            }
-            expected_request_body: dict = {
-                'id': 1234,
-                'jsonrpc': '2.0',
-                'method': 'icx_sendTransaction',
-                'params': {
-                    'data': self.setting["data"],
-                    'dataType': 'message',
-                    'from': self.setting["from"],
-                    'nid': hex(1),
-                    'signature': signed_transaction.signed_transaction_dict["signature"],
-                    'stepLimit': hex(self.setting["step_limit"]),
-                    'timestamp': hex(self.setting["timestamp"]),
-                    'to': self.setting["to"],
-                    'version': '0x3'
-                }
-            }
-            m.post(f"{BASE_DOMAIN_URL_V3_FOR_TEST}/api/v3", status_code=500, json=expected_result)
-            self.assertRaises(JSONRPCException, self.icon_service.send_transaction, signed_transaction)
-            actual_request_body = json.loads(m._adapter.last_request.text)
-            self.assertEqual(expected_request_body, actual_request_body)
+        self.assertRaises(JSONRPCException, self.icon_service.send_transaction, signed_transaction)
+
+        # When a sending address is wrong - not the wallet's address
+        message_transaction = MessageTransactionBuilder().from_(self.setting["to"]).to(self.setting["to"]) \
+            .step_limit(self.setting["step_limit"]).nid(self.setting["nid"]).data(self.setting["data"]).build()
+        signed_transaction = SignedTransaction(message_transaction, self.wallet)
+        self.assertRaises(JSONRPCException, self.icon_service.send_transaction, signed_transaction)
+
+
--- tests/providers/test_http_provider.py
+++ tests/providers/test_http_provider.py
@@ -16,11 +16,7 @@
 from unittest import main
 
 from iconsdk.exception import URLException
-<<<<<<< HEAD
-from iconsdk.providers.config_api_path import CONFIG_API_PATH
-=======
 from iconsdk.icon_service import IconService
->>>>>>> master
 from iconsdk.providers.http_provider import HTTPProvider
 from tests.api_send.test_send_super import TestSendSuper
 
@@ -30,39 +26,6 @@ class TestHTTPProvider(TestSendSuper):
     DEBUG_FULL_PATH_URL = "http://localhost:9000/api/debug/v3"
     BASE_PATH_URL = "http://localhost:9000"
     VERSION = 3
-<<<<<<< HEAD
-    request_kwargs: dict = {
-        'timeout': 60,
-        'allow_redirects': False,
-        'verify': True
-    }
-
-    def test_http_provider_base_url(self):
-        provider = HTTPProvider(
-            self.BASE_PATH_URL,
-            self.VERSION,
-            request_kwargs=self.request_kwargs
-        )
-        self.assertEqual(self.BASE_PATH_URL, provider._base_domain_url)
-        self.assertEqual(None, provider._full_path_url)
-        self.assertEqual(self.VERSION, provider._version)
-        self.assertEqual(self.request_kwargs, provider._request_kwargs)
-
-        # debug
-        for method in CONFIG_API_PATH:
-            url: str = provider._get_full_path_url(method=method)
-            self.assertEqual(self.DEBUG_FULL_PATH_URL, url)
-
-    def test_http_provider_full_url(self):
-        provider = HTTPProvider(
-            self.FULL_PATH_URL,
-            request_kwargs=self.request_kwargs
-        )
-        self.assertEqual(provider._full_path_url, self.FULL_PATH_URL)
-        self.assertEqual(provider._request_kwargs, self.request_kwargs)
-
-    def test_set_http_provider_validate_base_url(self):
-=======
 
     def test_set_http_provider_with_param(self):
         # the initializer
@@ -107,7 +70,6 @@ class TestHTTPProvider(TestSendSuper):
                 self.fail(f'Unexpected exception with {url}')
 
     def test_set_http_provider_by_new_initializer_with_invalid_url(self):
->>>>>>> master
         invalid_urls = [
             "http://localhost:9000/",
             "http://localhost:9000/api/v3",
@@ -116,15 +78,9 @@ class TestHTTPProvider(TestSendSuper):
             "https://ctz.solidwallet.io/",
             "https://ctz.solidwallet.io/api/v3",
         ]
-<<<<<<< HEAD
-        for invalid_url in invalid_urls:
-            with self.assertRaises(URLException):
-                HTTPProvider(invalid_url, self.VERSION)
-=======
         for url in invalid_urls:
             with self.assertRaises(URLException):
                 HTTPProvider(url, self.VERSION)
->>>>>>> master
 
 
 if __name__ == "__main__":
