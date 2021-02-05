df8375d2ca6f88bd0212990d99551b1d0226d156
byungkwon bae <endlessgate1@gmail.com>
2021-02-05 16:27:43 +0900

    get_transaction_result tests with mocking

--- tests/api_get/test_get_transaction_result.py
+++ tests/api_get/test_get_transaction_result.py
@@ -12,59 +12,111 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-from time import sleep
-from unittest import main
 
-from iconsdk.builder.transaction_builder import DeployTransactionBuilder
+import requests_mock
+import json
+
+from unittest import main
+from unittest.mock import patch
+
 from iconsdk.exception import DataTypeException, JSONRPCException
-from iconsdk.signed_transaction import SignedTransaction
-from iconsdk.utils.hexadecimal import remove_0x_prefix, add_cx_prefix, add_0x_prefix
+from iconsdk.utils.hexadecimal import remove_0x_prefix, add_cx_prefix
 from iconsdk.utils.validation import is_transaction_result
 from tests.api_send.test_send_super import TestSendSuper
+from tests.example_config import BASE_DOMAIN_URL_V3_FOR_TEST
 
 
+@patch('iconsdk.providers.http_provider.HTTPProvider._make_id', return_value=1234)
 class TestGetTransactionResult(TestSendSuper):
+    def test_get_transaction_result(self, _make_id):
+        tx_hash: str = "0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238"
+        with requests_mock.Mocker() as m:
+            expected_request = {
+                'id': 1234,
+                'jsonrpc': '2.0',
+                'method': 'icx_getTransactionResult',
+                'params': {
+                    'txHash': tx_hash
+                }
+            }
+            expected_result = {
+                "jsonrpc": "2.0",
+                "result": {
+                    "txHash": "0x33db06f38424207daa69c9df153649fd3913c21e162f16f4839c9c3318e44388",
+                    "blockHeight": "0x13f",
+                    "blockHash": "0x069e8a2431ae2c7e55924af477be87518476aa1eb1b2e7d1ee8d61d7874ea907",
+                    "txIndex": "0x1",
+                    "to": "cx0000000000000000000000000000000000000000",
+                    "stepUsed": "0x263b8",
+                    "stepPrice": "0x2540be400",
+                    "cumulativeStepUsed": "0x263b8",
+                    "eventLogs": [
+                        {
+                            "scoreAddress": "cx0000000000000000000000000000000000000000",
+                            "indexed": [
+                                "PRepSet(Address)"
+                            ],
+                            "data": [
+                                "hx86aba2210918a9b116973f3c4b27c41a54d5dafe"
+                            ]
+                        }
+                    ],
+                    "logsBloom": "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000080000000000000000000000000000000000000000000000000000000000020000000000000008000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+                    "status": "0x1"
+                },
+                "id": 1234
+            }
+            m.post(f"{BASE_DOMAIN_URL_V3_FOR_TEST}/api/v3/", json=expected_result)
+            result = self.icon_service.get_transaction_result(tx_hash)
+            actual_request = json.loads(m._adapter.last_request.text)
+            self.assertEqual(expected_request, actual_request)
+            self.assertTrue(is_transaction_result(result))
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
+    def test_get_transaction_result_wrong_hash(self, _make_id):
+        wrong_tx_hash: str = "0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238"
+        with requests_mock.Mocker() as m:
+            expected_request = {
+                'id': 1234,
+                'jsonrpc': '2.0',
+                'method': 'icx_getTransactionResult',
+                'params': {
+                    'txHash': wrong_tx_hash
+                }
+            }
+            expected_result = {
+                "jsonrpc": "2.0",
+                "error": {
+                    "code": -32602,
+                    "message": "Invalid params txHash"
+                },
+                "id": 1234
+            }
+            m.post(f"{BASE_DOMAIN_URL_V3_FOR_TEST}/api/v3/", status_code=400, json=expected_result)
+            # case 4: when tx_hash is invalid - not exist
+            self.assertRaises(JSONRPCException, self.icon_service.get_transaction_result, wrong_tx_hash)
+            actual_request = json.loads(m._adapter.last_request.text)
+            self.assertEqual(expected_request, actual_request)
 
-        param = {"init_supply": 10000}
-        deploy_transaction = DeployTransactionBuilder() \
-            .from_(cls.setting["from"]) \
-            .to(cls.setting["to_install"]) \
-            .step_limit(cls.setting["step_limit"]) \
-            .nid(cls.setting["nid"]) \
-            .nonce(cls.setting["nonce"]) \
-            .content_type(cls.setting["content_type"]) \
-            .content(cls.setting["content_install"]) \
-            .params(param) \
-            .version(3) \
-            .build()
-
-        # Test install SCORE : Sends transaction which makes the SCORE install correctly
-        signed_transaction_dict = SignedTransaction(deploy_transaction, cls.wallet)
-        tx_result = cls.icon_service.send_transaction(signed_transaction_dict)
-
-        sleep(2)
-
-        cls.tx_hash = tx_result
-
-    def test_validate_transaction(self):
-        result = self.icon_service.get_transaction_result(self.tx_hash)
-        self.assertTrue(is_transaction_result(result))
-
+    def test_get_transaction_result_invalid(self, _make_id):
+        invalid_tx_hash: str = "0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238"
         # case 1: when tx_hash is invalid - no prefixed
-        self.assertRaises(DataTypeException, self.icon_service.get_transaction_result, remove_0x_prefix(self.tx_hash))
+        self.assertRaises(
+            DataTypeException,
+            self.icon_service.get_transaction_result,
+            remove_0x_prefix(invalid_tx_hash)
+        )
         # case 2: when tx_hash is invalid - wrong prefixed
-        self.assertRaises(DataTypeException, self.icon_service.get_transaction_result,
-                          add_cx_prefix(remove_0x_prefix(self.tx_hash)))
+        self.assertRaises(
+            DataTypeException,
+            self.icon_service.get_transaction_result,
+            add_cx_prefix(remove_0x_prefix(invalid_tx_hash))
+        )
         # case 3: when tx_hash is invalid - too short
-        self.assertRaises(DataTypeException, self.icon_service.get_transaction_result, self.tx_hash[:15])
-        # case 4: when tx_hash is invalid - not exist
-        tx_hash_invalid = add_0x_prefix(os.urandom(32).hex())
-        self.assertRaises(JSONRPCException, self.icon_service.get_transaction_result, tx_hash_invalid)
+        self.assertRaises(
+            DataTypeException,
+            self.icon_service.get_transaction_result,
+            invalid_tx_hash[:15]
+        )
 
 
 if __name__ == "__main__":
